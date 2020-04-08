d73e02c8079a4519c5ea4de46581bb290cd94c31
John Sharpe <john.t.sharpe@gmail.com>
2020-04-07 21:01:03 -0700

    rename guess and guess_loop right

--- cs52/project2/guess.cpp
+++ cs52/project2/guess.cpp
@@ -6,48 +6,30 @@ using namespace std;
 
 int main()
 {
+  srand((unsigned)time(0));
+  int myRandNum;
+  int playerNum;
+  myRandNum = (rand()%100)+1;
+  cout << "Player please pick a number (0-100) ";
+  cin >> playerNum;
+  cout << "\n Dealer has " << myRandNum << "\n";
 
-  int dealerWon=0;
-  int playerWon=0;
-  while (true){
-    int myRandNum;
-    int playerNum;
-
-    myRandNum = (rand()%100)+1;
-    cout << "Player please pick a number (0-100) ";
-    cin >> playerNum;
-    if (playerNum==21){
-      break;
-    }
-
-    cout << "\n Dealer has " << myRandNum << "\n";
-
-    if (myRandNum > 21) {
-      myRandNum=myRandNum-21;
-    } else {
-      myRandNum=21-myRandNum;
-    }
-    if (playerNum > 21) {
-      playerNum=playerNum-21;
-    } else {
-      playerNum=21-playerNum;
-    }
-
-    if (playerNum < myRandNum){
-      cout << "Player wins\n";
-      playerWon++;
-    } else {
-      cout << "Dealer wins\n";
-      dealerWon++;
-    }
+  if (myRandNum > 21) {
+    myRandNum=myRandNum-21;
+  } else {
+    myRandNum=21-myRandNum;
+  }
+  if (playerNum > 21) {
+    playerNum=playerNum-21;
+  } else {
+    playerNum=21-playerNum;
   }
 
-  cout << "Number of hands played: " << playerWon+dealerWon << "\n";
-  cout << "Dealer won: " << dealerWon << "\n";
-  cout << "Player won: " << playerWon << "\n";
-  cout << "You’re " << playerWon << " for " << dealerWon << "\n";
-  cout << "    Come back to the CS052\nRock on.\n";
-  /* printf? */
-
+  if (playerNum < myRandNum){
+    cout << "Player wins\n";
+  } else {
+    cout << "Dealer wins\n";
+  }
 }
+
 
--- cs52/project2/guess_loop.cpp
+++ cs52/project2/guess_loop.cpp
@@ -6,30 +6,48 @@ using namespace std;
 
 int main()
 {
-  srand((unsigned)time(0));
-  int myRandNum;
-  int playerNum;
-  myRandNum = (rand()%100)+1;
-  cout << "Player please pick a number (0-100) ";
-  cin >> playerNum;
-  cout << "\n Dealer has " << myRandNum << "\n";
 
-  if (myRandNum > 21) {
-    myRandNum=myRandNum-21;
-  } else {
-    myRandNum=21-myRandNum;
-  }
-  if (playerNum > 21) {
-    playerNum=playerNum-21;
-  } else {
-    playerNum=21-playerNum;
+  int dealerWon=0;
+  int playerWon=0;
+  while (true){
+    int myRandNum;
+    int playerNum;
+
+    myRandNum = (rand()%100)+1;
+    cout << "Player please pick a number (0-100) ";
+    cin >> playerNum;
+    if (playerNum==21){
+      break;
+    }
+
+    cout << "\n Dealer has " << myRandNum << "\n";
+
+    if (myRandNum > 21) {
+      myRandNum=myRandNum-21;
+    } else {
+      myRandNum=21-myRandNum;
+    }
+    if (playerNum > 21) {
+      playerNum=playerNum-21;
+    } else {
+      playerNum=21-playerNum;
+    }
+
+    if (playerNum < myRandNum){
+      cout << "Player wins\n";
+      playerWon++;
+    } else {
+      cout << "Dealer wins\n";
+      dealerWon++;
+    }
   }
 
-  if (playerNum < myRandNum){
-    cout << "Player wins\n";
-  } else {
-    cout << "Dealer wins\n";
-  }
+  cout << "Number of hands played: " << playerWon+dealerWon << "\n";
+  cout << "Dealer won: " << dealerWon << "\n";
+  cout << "Player won: " << playerWon << "\n";
+  cout << "You’re " << playerWon << " for " << dealerWon << "\n";
+  cout << "    Come back to the CS052\nRock on.\n";
+  /* printf? */
+
 }
-
 
