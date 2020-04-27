4071836c95d647d39d7d42b23d3c93bf71a8a85b
John Sharpe <john.t.sharpe@gmail.com>
2020-04-26 17:23:52 -0700

    project3

--- cs52/project3/bitcoin.cpp
+++ cs52/project3/bitcoin.cpp
@@ -1,87 +1,161 @@
+#include <ctime>
 #include <iostream>
 #include <stdlib.h>
-#include <string.h>
-#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 int myMenu(){
   int myChoice;
-  cout << "Enter a number:\n";
+  cout << endl;
   cout << "1. Print the current value of bitcoin.\n";
   cout << "2. Buy bitcoin.\n";
   cout << "3. Sell bitcoins\n";
   cout << "4. Print your bitcoin balance.\n";
   cout << "5. Print your transaction history.\n";
+  cout << "Enter a number: ";
   cin >> myChoice;
   return myChoice;
 }
 
+
+class Wallet {
+public:
+  string ticker="BTC";
+  int currentCoins;
+  void printWallet(){
+    cout << ticker << ":" << currentCoins << endl;
+  }
+};
+
+class Ledger {
+public:
+  vector<string> ledger;
+  void storeTransaction(string myEntry){
+    ledger.push_back(myEntry);
+  }
+
+  void printLedger(){
+    unsigned i;
+    for (i=0; i<ledger.size();i++){
+      cout << ledger.at(i) << endl;
+    }
+  }
+};
+
+class myDate {
+public:
+  string stringDate(){
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    string s(dt);
+    s.erase(s.find_last_not_of(" \n\r\t")+1);
+    return s;
+  }
+};
+
+class getLive {
+public:
+  int btcPrice(){
+    srand (time(NULL));
+    int b = rand() % 1000 + 5000;
+    return b;
+  }
+};
+
+string toString(auto &i){
+  // https://stackoverflow.com/questions/5590381/easiest-way-to-convert-int-to-string-in-c
+  // https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+  stringstream ss;
+  ss << i;
+  string s;
+  s=ss.str();
+  s.erase(s.find_last_not_of(" \n\r\t")+1);
+  return s;
+}
+
 int main(){
 
-  /*
-    MENU
-  1. The user can ask for the current value of bitcoin (just use a default value initially of 5400k).
-  2. They can buy bitcoin.
-  3. They can sell bitcoin.
-  4. They can print their balance (show their current USD, the number of bitcoins they have, and USD value of the bitcoins they have)
-  5. They can print the history of their buy and sell transactions out. This should have the time, which you can format however you want ie: bought 1 bitcoin on September 12, 2019.
-  */
-
+  int myDollars=20000;
   int bitCoinValue=5400;
-  int bitCoinBalance=0;
   int myChoice=0;
+
+  Wallet myWallet;
+  Ledger myLedger;
+
+  myWallet.currentCoins=0;
+
+  myDate m;
+  cout << m.stringDate();
 
   while (myChoice!=6){
     myChoice = myMenu();
+
     switch(myChoice) {
+
     case 1:
-      cout << "choice 1" << endl;
+      cout << "The current value of Bitcoin is " << bitCoinValue << endl;
+      getLive g;
+      bitCoinValue=g.btcPrice();
+      cout << "The new value of Bitcoin is " << bitCoinValue << endl;
       break;
+
     case 2:
-      cout << "choice 2" << endl;
+      int buyBTC;
+      cout << "Buy how many Bitcoin? ";
+      cin >> buyBTC;
+      int myCost;
+      myCost=buyBTC*bitCoinValue;
+      if (myCost > myDollars) {
+        cout << "No bitcoin bought, you don't have enough $$$\n"; 
+      } else {
+        myWallet.currentCoins+=buyBTC;
+        myDollars-=myCost;
+        cout << "My current number of bitcoins: " << myWallet.currentCoins << endl;
+        cout << "My current USDs: " << myDollars << endl;
+        myDate d;
+        string myMessage;
+        myMessage=d.stringDate() + ": " + "Bought " + toString(buyBTC) + " BTC. for " + toString(bitCoinValue) + " My cash " + toString(myDollars) + " My BTC " + toString(myWallet.currentCoins);
+        myLedger.ledger.push_back(myMessage);
+      }
+
       break;
+
     case 3:
-      cout << "choice 3" << endl;
+      int sellBTC;
+      cout << "Sell how many Bitcoin? ";
+      cin >> sellBTC;
+      if (sellBTC > myWallet.currentCoins) {
+        cout << "You can't sell more bitcoins than " << myWallet.currentCoins << endl;
+      } else {
+        myWallet.currentCoins-=sellBTC;
+        myDollars+=sellBTC*bitCoinValue;
+        cout << "My current number of bitcoins: " << myWallet.currentCoins << endl;
+        cout << "My current USDs: " << myDollars << endl;
+        myDate d;
+        string myMessage;
+        myMessage=d.stringDate() + ": " + "Sold " + toString(sellBTC) + " BTC. for " + toString(bitCoinValue) + " My cash " + toString(myDollars) + " My BTC " + toString(myWallet.currentCoins);
+        myLedger.ledger.push_back(myMessage);
+      }
       break;
+
     case 4:
-      cout << "choice 4" << endl;
+      cout << "My current bitcoins are: " << myWallet.currentCoins << endl;
+      cout << "My current USDs: " << myDollars << endl;
       break;
+
     case 5:
-      cout << "choice 5" << endl;
+      cout << "My transaction history:" << endl;
+      unsigned int i;
+      for (i=0;i<myLedger.ledger.size();i++) {
+        cout << myLedger.ledger.at(i) << endl;
+      }
       break;
     default:
       cout << "invalid" << endl;
     }
   }
-
-
-
-
-  class Wallet {
-    
-  };
-
-  class Ledger {
-    
-  };
-
-  class myDate {
-  public:
-    string returnDate(){
-      locale::global(std::locale("en_US.UTF-8"));
-      time_t t = time(nullptr);
-      char mbstr[100];
-      if (strftime(mbstr, sizeof(mbstr), "%A %c", localtime(&t))) {
-        return mbstr;
-      } else {
-        return "error";
-      }
-    }
-  };
-
-
-
-
 
 }
